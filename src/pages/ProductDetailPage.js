02477bf284f12691a81065d3185f70a4d130d205
Abdullah Asad <74613117+abdullah16aug@users.noreply.github.com>
2023-07-01 20:42:50 +0530

    setup redux

---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ProductDetail from '../features/product-list/components/ProductDetail'
+const ProductDetailPage = () => {
+  return (
+    <ProductDetail/>
+  ) 
+}
+
+export default ProductDetailPage
